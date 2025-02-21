449c097fe38a23776a1cbe39e7900148c4aa9300
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-02-21 20:06:51 +0330

    feat(date-picker): show date-picker in side column

--- src/app/features/calendar/components/date-picker/date-picker.component.ts
+++ src/app/features/calendar/components/date-picker/date-picker.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
-
+import {Component, effect, model} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { Router } from '@angular/router';
+import { CalendarRoutes } from '@enums';
 @Component({
   selector: 'app-date-picker',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatDatepickerModule
+  ],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss'
 })
 export class DatePickerComponent {
+  selectedDate = model<Date | null>(null);
 
+  constructor(private router: Router) {
+
+    effect(() => {
+      const date = this.selectedDate();
+      if (date) {
+        this.onDateSelected(date);
+      }
+    })
+  }
+
+  onDateSelected(date: Date) {
+    this.selectedDate.set(date);
+    if (date) {
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      
+      this.router.navigate(['/', CalendarRoutes.CALENDAR, CalendarRoutes.DAY, year, month, day]);
+    }
+  }
 }
