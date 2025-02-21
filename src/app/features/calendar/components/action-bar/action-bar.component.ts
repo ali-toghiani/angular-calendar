a6cffc519b2bccb84ede22a9d05f394a85e4de30
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-02-21 20:06:30 +0330

    feat(action-bar): show current date in action-bar

--- src/app/features/calendar/components/action-bar/action-bar.component.ts
+++ src/app/features/calendar/components/action-bar/action-bar.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, model, OnInit } from '@angular/core';
+import { MatIconModule, MatIconRegistry} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-action-bar',
-  imports: [],
+  imports: [
+    MatIconModule,
+     MatButtonModule,
+     CommonModule
+    ],
   templateUrl: './action-bar.component.html',
   styleUrl: './action-bar.component.scss'
 })
-export class ActionBarComponent {
+export class ActionBarComponent implements OnInit{
 
+  date = model<Date | null>(null);
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'calendar',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/google-calendar.svg')
+    );
+  }
+
+  ngOnInit(): void {
+    this.route.firstChild?.params.subscribe(params => {
+      console.log('Child route params:', params);
+      this.date.set(new Date(+params['year'], +params['month'], +params['day']));
+    });
+  }
 }
