a638bcbfb45c611d56baf828ab87b1dbd6ca4ae4
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-02-23 14:00:17 +0330

    ref(services/appointment.service): refactor and improve code

--- src/app/services/appointment.service.ts
+++ src/app/services/appointment.service.ts
@@ -3,98 +3,136 @@ import { DateService } from './date.service';
 import { Appointment, AppointmentDTO, AppointmentsByDate } from '@models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppointmentService {
   storedAppointments = signal<Record<string, Appointment[]>>({});
 
   private readonly STORAGE_KEY = 'appointments';
 
-  constructor(
-    private dateService: DateService
-  ) {
+  constructor(private dateService: DateService) {
     this.loadAppointments();
   }
 
   loadAppointments(): void {
-    const storageData = localStorage.getItem(this.STORAGE_KEY);
-    if (!storageData) {return}
-    const parsedDto = JSON.parse(storageData) as { string: AppointmentDTO[]};
-    const mappedDto = this.mapStorageToAppointments(parsedDto);
-    this.storedAppointments.set(mappedDto);
-  }
+    try {
+      const storageData = localStorage.getItem(this.STORAGE_KEY);
+      if (!storageData) return;
 
-  mapStorageToAppointments(storage: Record<string, AppointmentDTO[]>): Record<string, Appointment[]>{
-    return Object.fromEntries(
-      Object.entries(storage).map(([key, arrayOfObjects]) => [
-        key,
-        arrayOfObjects.map(item => {
-          return {
-            ...item,
-            start: new Date(item.start),
-            end: new Date(item.end)
-          };
-        })
-      ])
-    );
-  }
+      const parsedDto = JSON.parse(storageData) as Record<string, AppointmentDTO[]>;
+      const mappedDto = this.mapStorageToAppointments(parsedDto);
+      this.storedAppointments.set(mappedDto);
+    } catch (error) {
+      // TODO: calll LogService
+      console.error('Error loading appointments', error);
 
-  mapAppointmentsToStorage(data: Record<string, Appointment[]>): Record<string, AppointmentDTO[]>{
-    return Object.fromEntries(
-      Object.entries(data).map(([key, arrayOfObjects]) => [
-        key,
-        arrayOfObjects.map(item => {
-          return {
-            ...item,
-            start: item.start.toISOString(),
-            end: item.end.toISOString()
-          };
-        })
-      ])
-    );
+      // optional: clear local storage
+      this.storedAppointments.set({});
+    }
   }
 
   storeAppointment(appointment: Appointment): void {
-    const dayId = this.dateService.getDateId(appointment.start);
-    const storedItems = this.storedAppointments()
-    if (!dayId) return;
-    const targetDayAppointments = this.storedAppointments()[dayId];
-    if (targetDayAppointments) {
-      if (this.isAppointmentDuplicate(appointment, targetDayAppointments)) {
-        return;
-      }
-      storedItems[dayId].push(appointment);
-    } else {
-      storedItems[dayId] = [appointment];
-    }
-    const mappedStorage = this.mapAppointmentsToStorage(storedItems);
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(mappedStorage, null, 2));
-    this.loadAppointments();
-  }
+    try {
+      const dayId = this.dateService.getDateId(appointment.start);
+      if (!dayId) return;
 
-  private isAppointmentDuplicate(appointment: Appointment, current: Appointment[]): boolean {
-    return current.findIndex( (item:Appointment) => item.start.getTime() === appointment.start.getTime() && item.end.getTime() === appointment.end.getTime()) !== -1;
+      const storedItems = { ...this.storedAppointments() };
+      const targetDayAppointments = storedItems[dayId];
+
+      if (targetDayAppointments) {
+        if (this.isAppointmentDuplicate(appointment, targetDayAppointments)) {
+          return;
+        }
+        storedItems[dayId] = [...targetDayAppointments, appointment];
+      } else {
+        storedItems[dayId] = [appointment];
+      }
+
+      this.saveToLocalStorage(storedItems);
+
+    } catch (error) {
+      console.error('Error in storeAppointment:', error);
+    }
   }
 
   deleteAppointment(id: string, dateId: string): void {
-    const storedItems = this.storedAppointments()
-    const targetDayAppointments = this.storedAppointments()[dateId];
-    if (targetDayAppointments) {
-      storedItems[dateId] = storedItems[dateId].filter( item => item.id !== id)
-    }
-    const mappedStorage = this.mapAppointmentsToStorage(storedItems);
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(mappedStorage, null, 2));
-    this.loadAppointments();
+    this.updateStoredItems(
+      dateId,
+      items => items.filter(item => item.id !== id)
+    );
   }
 
   updateAppointment(appointment: Appointment, dateId: string): void {
-    const storedItems = this.storedAppointments()
-    const targetDayAppointments = this.storedAppointments()[dateId];
-    if (targetDayAppointments) {
-      storedItems[dateId] = storedItems[dateId].map( item => item.id == appointment.id ? appointment : item);
+    this.updateStoredItems(
+      dateId,
+      items => items.map(item => 
+        item.id === appointment.id ? appointment : item
+      )
+    );
+  }
+
+  private saveToLocalStorage(data: Record<string, Appointment[]>): void {
+    try {
+      const mappedStorage = this.mapAppointmentsToStorage(data);
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(mappedStorage, null, 2));
+      this.loadAppointments();
+    } catch (error) {
+      // TODO: calll LogService
+      console.error('Error saving appointments', error);
     }
-    const mappedStorage = this.mapAppointmentsToStorage(storedItems);
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(mappedStorage, null, 2));
-    this.loadAppointments();
+  }
+
+  private mapStorageToAppointments(
+    data: Record<string, AppointmentDTO[]>
+  ): Record<string, Appointment[]> {
+    return Object.fromEntries(
+      Object.entries(data).map(([key, arrayOfObjects]) => [
+        key,
+        arrayOfObjects.map((item) => (
+          {
+            ...item,
+            start: new Date(item.start),
+            end: new Date(item.end),
+          }
+        )),
+      ])
+    );
+  }
+
+  private mapAppointmentsToStorage(
+    data: Record<string, Appointment[]>
+  ): Record<string, AppointmentDTO[]> {
+    return Object.fromEntries(
+      Object.entries(data).map(([key, arrayOfObjects]) => [
+        key,
+        arrayOfObjects.map((item) => (
+          {
+            ...item,
+            start: item.start.toISOString(),
+            end: item.end.toISOString(),
+          }
+        )),
+      ])
+    );
+  }
+
+  private updateStoredItems(
+    dateId: string, 
+    updateFn: (items: Appointment[]) => Appointment[]
+  ): void {
+    const storedItems = this.storedAppointments();
+    const targetDayAppointments = storedItems[dateId];
+    
+    if (targetDayAppointments) {
+      storedItems[dateId] = updateFn(targetDayAppointments);
+      this.saveToLocalStorage(storedItems);
+    }
+  }
+
+  private isAppointmentDuplicate(appointment: Appointment, current: Appointment[]): boolean {
+    return current.some(item => 
+      item.start.getTime() === appointment.start.getTime() && 
+      item.end.getTime() === appointment.end.getTime()
+    );
   }
 }
